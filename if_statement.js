5614c93f558f6f5645fac9694a656ce91836d62f
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-01 17:06:46 -0500

    Add if statement to check the value of a number

---
+++ if_statement.js
@@ -0,0 +1,10 @@
+var number = 2;
+if (number <= 5) {
+  console.log("The number is less than or equal to 5");
+}
+else if (number > 5 && number <= 10) {
+  console.log("The number is greater than 5 and less than or equal to 10");
+}
+else {
+    console.log("The number is greater than 10");
+}

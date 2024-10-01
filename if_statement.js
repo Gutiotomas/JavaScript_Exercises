f6b2eff4441135053b966fbbb7a0e152e8f9d925
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-01 17:11:50 -0500

    Add switch case to check the value of a number

--- if_statement.js
+++ if_statement.js
@@ -8,3 +8,20 @@ else if (number > 5 && number <= 10) {
 else {
     console.log("The number is greater than 10");
 }
+
+// or alternatively 
+
+var num =7;
+switch (true) {
+    case (num <= 5): {
+        console.log("The number is less than or equal to 5");
+        break;
+    }
+    case (num > 5 && num <= 10): {
+        console.log("The number is greater than 5 and less than or equal to 10");
+        break;
+    }
+    default: {
+        console.log("The number is greater than 10");
+    }
+}

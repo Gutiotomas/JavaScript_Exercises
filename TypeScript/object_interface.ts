0e90553fcd53b55c3ac7a7555539784aed49367c
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-05 22:08:48 -0500

    Add object_interface.ts with persona variable and Persona interface

---
+++ TypeScript/object_interface.ts
@@ -0,0 +1,15 @@
+let persona: { nombre: string; edad: number } = {
+    nombre: "Tomás",
+    edad: 22
+};
+
+
+interface Persona {
+    nombre: string;
+    edad: number;
+}
+
+const usuario: Persona = {
+    nombre: "Tomás",
+    edad: 22
+};

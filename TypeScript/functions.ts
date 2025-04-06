89ff3438890a5eff8e28fda23fb0ad360db947cd
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-05 22:27:47 -0500

    Add for loop examples and object iteration in TypeScript

--- TypeScript/functions.ts
+++ TypeScript/functions.ts
@@ -5,5 +5,7 @@ function saludar(nombre: string): string {
 const saludo = saludar("Tomás");
 
 function multiplicar(a: number, b?: number): number {
-    return b ? a * b : a;
+    return b ? a * b : a;  //condición ? valor_si_true : valor_si_false;
+    // si b es undefined, se multiplica solo a
+    // si b tiene un valor, se multiplica a por b
 }

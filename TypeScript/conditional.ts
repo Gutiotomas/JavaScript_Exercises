fa8efd05e9e578a90d782329c2ae2a4745f942af
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-05 22:11:37 -0500

    Add conditional statements using if-else and switch in TypeScript

---
+++ TypeScript/conditional.ts
@@ -0,0 +1,28 @@
+let number1: number = 2;
+if (number1 <= 5) {
+  console.log("The number is less than or equal to 5");
+}
+else if (number1 > 5 && number1 <= 10) {
+  console.log("The number is greater than 5 and less than or equal to 10");
+}
+else {
+    console.log("The number is greater than 10");
+}
+
+// or alternatively 
+
+let num2: number = 8;
+
+switch (true) {  // Use true to evaluate the conditions within each case
+    case (num2 <= 5): {
+        console.log("The number is less than or equal to 5");
+        break;
+    }
+    case (num2 > 5 && num2<= 10): {
+        console.log("The number is greater than 5 and less than or equal to 10");
+        break;
+    }
+    default: {
+        console.log("The number is greater than 10");
+    }
+}

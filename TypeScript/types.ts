69d9627f1efb4e336f991e29df4bb07a5428fb07
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-05 21:57:01 -0500

    Add initial TypeScript types and variable declarations

---
+++ TypeScript/types.ts
@@ -0,0 +1,10 @@
+const nombre: string = "Tomás";
+let edad: number = 23;
+let soltero: boolean = true;
+// any	Cualquier tipo (no recomendado)
+// unknown	Similar a any, pero más seguro
+// null, undefined	También están disponibles
+// void	No tiene valor de retorno (funciones)
+// never	No tiene valor (funciones que nunca terminan)
+// bigint	Números enteros grandes (mayores que Number.MAX_SAFE_INTEGER)
+// Enums	Conjuntos de valores constantes (números o cadenas)

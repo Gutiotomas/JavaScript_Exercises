f0171552dc9fd38442ad149566f84dcb7811ec2a
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-08 09:52:14 -0500

    Add TypeScript implementation of esAnagrama function to check if two strings are anagrams

---
+++ TypeScript/dist/algorithms/anagrama.js
@@ -0,0 +1,20 @@
+"use strict";
+function esAnagrama(a, b) {
+    if (a.length !== b.length)
+        return false;
+    const conteo = {};
+    for (const letra of a) {
+        conteo[letra] = (conteo[letra] || 0) + 1;
+    }
+    for (const letra of b) {
+        if (!conteo[letra])
+            return false;
+        conteo[letra]--;
+    }
+    return true;
+}
+console.log(esAnagrama("listen", "silent")); // true
+console.log(esAnagrama("hola", "aloh")); // true
+console.log(esAnagrama("rat", "car")); // false
+console.log(esAnagrama("tomás", "samtó")); // true
+console.log(esAnagrama("tomas", "samto"));

05a6a4133be2c2b24a3b4f64e896c1999faebb18
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-17 17:52:52 -0500

    Update TypeScript configuration to target ES2020 and add Fibonacci functions implementation

---
+++ TypeScript/algorithms/fibonacci.ts
@@ -0,0 +1,64 @@
+//Return a fibonacci sequence as an array.
+
+//If the Fibonacci wants to start from 1, then the first two numbers should be 1 and 1.
+
+function fibonacci(n: number): number[] {
+  const fibSequence: number[] = [0, 1];
+
+  let currentValue: number = 1;
+  let previousValue: number = 0;
+
+  if (n === 1) {
+    return [0];
+  }
+
+  if (n === 2) {
+    return fibSequence;
+  }
+
+  let iteratorCounter: number = n - 2;
+
+  while (iteratorCounter) {
+    currentValue += previousValue;
+    previousValue = currentValue - previousValue;
+    fibSequence.push(currentValue);
+    iteratorCounter -= 1;
+  }
+
+  return fibSequence;
+}
+
+//Calculate fibonacci number at specific position using Dynamic Programming approach.
+
+//If the Fibonacci wants to start from 1, then the first two numbers should be 1 and 1.
+
+function fibonacciNth(n: number): bigint {
+  let currentValue1: bigint = 1n;
+  let previousValue1: bigint = 0n;
+
+  if (n === 1) {
+    return 0n;
+  }
+
+  if (n === 2) {
+    return 1n;
+  }
+
+  let iterationsCounter1: number = n - 2;
+
+  while (iterationsCounter1) {
+    currentValue1 += previousValue1;
+    previousValue1 = currentValue1 - previousValue1;
+
+    iterationsCounter1 -= 1;
+  }
+
+  return currentValue1;
+}
+
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(10));
+console.log(fibonacciNth(1));
+console.log(fibonacciNth(2));
+console.log(fibonacciNth(10));

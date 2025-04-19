67a3f1444c718ce0cb20845a6a5df3a54404b834
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-18 19:32:42 -0500

    Add indiceMaximo function to find the index of the maximum number in an array

---
+++ TypeScript/excersices/mayor_index.ts
@@ -0,0 +1,15 @@
+function indiceMaximo(arr: number[]): number {
+    let maxNumber = 0;
+    let maxIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > maxNumber) {
+            maxIndex = i;
+            maxNumber = arr[i];
+        }
+    }    
+    return maxIndex;
+}
+
+console.log(indiceMaximo([1, 2, 3, 4, 5])); // 4
+console.log(indiceMaximo([51, 14, 23, 42, 10])); // 0
+console.log(indiceMaximo([34, 31, 42, 4, 25])); // 2

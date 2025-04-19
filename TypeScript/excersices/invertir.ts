1e880731902ae4df8de4aae16c36b4d24d2c9ac5
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-18 19:03:20 -0500

    Add invertirArray function to reverse an array and provide usage example

---
+++ TypeScript/excersices/invertir.ts
@@ -0,0 +1,11 @@
+function invertirArray(arr: number[]): number[] {
+    let invertido = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        invertido.push(arr[i]);
+    }
+    return invertido;
+}
+
+// Ejemplo de uso
+let arrayOriginal = [1, 2, 3, 4, 5];
+console.log(arrayOriginal,invertirArray(arrayOriginal)); // [5, 4, 3, 2, 1]

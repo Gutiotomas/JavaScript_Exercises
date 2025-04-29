76855a73ee294f48c7c08138c5183862b7a18634
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-04-29 11:16:19 -0500

    Refactor esPalindromo function to use a two-pointer approach for palindrome checking

--- TypeScript/algorithms/palindrome.ts
+++ TypeScript/algorithms/palindrome.ts
@@ -1,9 +1,15 @@
 function esPalindromo(cadena: string): boolean {
   const limpia = cadena.toLowerCase().replace(/[^a-z0-9]/g, ""); // quita todo lo que no sea letra o número
-
-  const invertida = limpia.split("").reverse().join("");
-
-  return limpia === invertida;
+  let start = 0;
+  let end = limpia.length - 1;
+  while (start < end) {
+    if (limpia[start] !== limpia[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 }
 
 function isPalindrome(arr: number[]): boolean {
@@ -11,12 +17,12 @@ function isPalindrome(arr: number[]): boolean {
   let end = arr.length - 1;
   while (start < end) {
     if (arr[start] != arr[end]) {
-      return false
+      return false;
     }
     start++;
     end--;
   }
-  return true
+  return true;
 }
 
 console.log(esPalindromo("anita lava la tina")); // true
@@ -25,6 +31,3 @@ console.log(esPalindromo("Hola mundo")); // false
 console.log(esPalindromo("A man, a plan, a canal: Panama")); // true
 console.log(isPalindrome([1, 3, 5, 2])); // false
 console.log(isPalindrome([1, 3, 5, 3, 1])); // true
-
-
-
